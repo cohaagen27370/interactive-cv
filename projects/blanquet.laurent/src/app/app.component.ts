33fd6eb433203e226d7a51c46cf7b7d64c62f1eb
Laurent BLANQUET <46500674+cohaagen27370@users.noreply.github.com>
2026-04-06 10:23:38 +0200

    suppression du tracking service

--- projects/blanquet.laurent/src/app/app.component.ts
+++ projects/blanquet.laurent/src/app/app.component.ts
@@ -1,6 +1,6 @@
-import {Component, inject, OnInit} from '@angular/core';
-import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
-import {ClarityModule} from '@clr/angular';
+import { Component, inject, OnInit } from "@angular/core";
+import { RouterLink, RouterLinkActive, RouterOutlet } from "@angular/router";
+import { ClarityModule } from "@clr/angular";
 import {
   announcementIcon,
   certificateIcon,
@@ -9,70 +9,95 @@ import {
   organizationIcon,
   userIcon,
   cpuIcon,
-  briefcaseIcon
-} from '@cds/core/icon';
+  briefcaseIcon,
+} from "@cds/core/icon";
 import {
   trigger,
   transition,
   style,
   animate,
   query,
-  group
-} from '@angular/animations';
-import {CommonModule} from '@angular/common';
-import {globalStore} from '../global.store';
-import {TrackingService} from '../services/tracking.service';
-import {lastValueFrom} from 'rxjs';
+  group,
+} from "@angular/animations";
+import { CommonModule } from "@angular/common";
+import { globalStore } from "../global.store";
+import { TrackingService } from "../services/tracking.service";
+import { lastValueFrom } from "rxjs";
 
-ClarityIcons.addIcons(formIcon, userIcon, certificateIcon, organizationIcon,briefcaseIcon, announcementIcon, cpuIcon);
+ClarityIcons.addIcons(
+  formIcon,
+  userIcon,
+  certificateIcon,
+  organizationIcon,
+  briefcaseIcon,
+  announcementIcon,
+  cpuIcon,
+);
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet, CommonModule,
-    ClarityModule, RouterLink, RouterLinkActive],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  selector: "app-root",
+  imports: [
+    RouterOutlet,
+    CommonModule,
+    ClarityModule,
+    RouterLink,
+    RouterLinkActive,
+  ],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.scss",
   animations: [
-    trigger('routeAnimations', [
-      transition('* <=> *', [ // S'applique à toutes les transitions de route
-        style({ position: 'relative' }),
-        query(':enter, :leave', [
-          style({
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            width: '100%'
-          })
-        ], { optional: true }),
-        query(':enter', [
-          style({ opacity: 0 }) // Commence avec une opacité de 0
-        ], { optional: true }),
+    trigger("routeAnimations", [
+      transition("* <=> *", [
+        // S'applique à toutes les transitions de route
+        style({ position: "relative" }),
+        query(
+          ":enter, :leave",
+          [
+            style({
+              position: "absolute",
+              top: 0,
+              left: 0,
+              width: "100%",
+            }),
+          ],
+          { optional: true },
+        ),
+        query(
+          ":enter",
+          [
+            style({ opacity: 0 }), // Commence avec une opacité de 0
+          ],
+          { optional: true },
+        ),
         group([
-          query(':leave', [
-            animate('300ms ease-out', style({ opacity: 0 })) // L'ancienne page disparaît rapidement
-          ], { optional: true }),
-          query(':enter', [
-            animate('500ms ease-in', style({ opacity: 1 })) // La nouvelle page apparaît progressivement
-          ], { optional: true })
-        ])
-      ])
-    ])
-  ]
+          query(
+            ":leave",
+            [
+              animate("300ms ease-out", style({ opacity: 0 })), // L'ancienne page disparaît rapidement
+            ],
+            { optional: true },
+          ),
+          query(
+            ":enter",
+            [
+              animate("500ms ease-in", style({ opacity: 1 })), // La nouvelle page apparaît progressivement
+            ],
+            { optional: true },
+          ),
+        ]),
+      ]),
+    ]),
+  ],
 })
-export class AppComponent implements OnInit{
-
-  trackService = inject(TrackingService);
-
-  async ngOnInit() {
-    const ip = await lastValueFrom(this.trackService.getPublicIpAddress());
-    await lastValueFrom(this.trackService.track(ip));
-  }
-
+export class AppComponent {
   $store = inject(globalStore);
   nowYear: number = new Date().getFullYear();
 
   prepareRoute(outlet: RouterOutlet) {
-    return outlet && outlet.activatedRouteData && outlet.activatedRouteData['animation'];
+    return (
+      outlet &&
+      outlet.activatedRouteData &&
+      outlet.activatedRouteData["animation"]
+    );
   }
-
 }
