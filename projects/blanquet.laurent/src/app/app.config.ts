9076740a22f9e9f66c077b78f2c34507f0038159
Laurent BLANQUET <46500674+cohaagen27370@users.noreply.github.com>
2026-06-08 15:19:34 +0200

    adding missing files

--- projects/blanquet.laurent/src/app/app.config.ts
+++ projects/blanquet.laurent/src/app/app.config.ts
@@ -87,7 +87,7 @@ export const appConfig: ApplicationConfig = {
       withViewTransitions()
     ),
     provideIndexedDb(dbConfig),
-    provideClientHydration(withEventReplay()),
+    //provideClientHydration(withEventReplay()),
     provideAnimationsAsync()
   ]
 };
